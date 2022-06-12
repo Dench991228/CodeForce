5eaa2936495c5f3f055601095030b2555c3151b7
Dench991228 <denghanzhi85139@163.com>
2022-06-12 21:45:28 +0800

    完成了1686B

--- 1686B.cpp
+++ 1686B.cpp
@@ -9,6 +9,17 @@ int main(){
 		int length_permutation;
 		cin >> length_permutation;
 		int dp[length_permutation];// 从最初到这里最多可以完成几次odd划分
+		// dp[i] = max(dp[i-1], dp[i-2]+(arr[i-1]>arr[i]))
+		int *arr = (int*)malloc(sizeof(int) * length_permutation);
+		for(int i = 0; i < length_permutation; i++){
+			cin >> arr[i];
+		}
+		dp[0] = 0;
+		dp[1] = arr[0]>arr[1]?1:0;
+		for(int i = 2; i < length_permutation;i++){
+			dp[i] = max(dp[i-1], dp[i-2] + (arr[i-1] > arr[i] ? 1:0));
+		}
+		cout << dp[length_permutation-1] << endl;
 	}
   	return 0;
 }
